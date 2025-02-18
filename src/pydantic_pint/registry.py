6ec50a74e4b75ef68cd87e80c4fe07620bbec486
ernie <ernim@dtu.dk>
2025-02-18 12:46:26 +0100

    proposed clash solution

--- src/pydantic_pint/registry.py
+++ src/pydantic_pint/registry.py
@@ -4,4 +4,20 @@ from __future__ import annotations
 
 import pint
 
-DEFAULT_UNIT_REGISTRY = pint.UnitRegistry()
+_registry = pint.UnitRegistry()
+
+
+def get_unit_registry() -> pint.UnitRegistry:
+    """
+    Returns the current unit registry to be used.
+    Always use this function to retrieve the unit registry.
+    """
+    return _registry
+
+
+def init_unit_registry(registry: pint.UnitRegistry) -> None:
+    """
+    Overrides the global unit registry with the provided registry.
+    """
+    global _registry
+    _registry = registry
