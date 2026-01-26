c23758dd7f0337329b952aae95d57104be129910
Tyler Hughes <tylerhx111+git@proton.me>
2026-01-25 21:04:26 -0500

    refactored `PydanticPintValue` class into a function called `pydantic_pint_value`
    - this solves the mypy type issues regarding overriding `__new__`

--- src/pydantic_pint/value.py
+++ src/pydantic_pint/value.py
@@ -9,50 +9,67 @@ from pydantic_core import SchemaSerializer, core_schema
 
 from pydantic_pint.registry import get_registry
 
+__all__ = [
+    "pydantic_pint_value",
+]
 
-class PydanticPintValue:
-    """Proxy class for a Pint Quantity instance with pydantic serialization.
 
-    Unlink `PydanticPintQuantity`, `PydanticPintValue` wraps an instance of a pint quantity.
-    Methods are added to allow it to interact with pydantic, e.g. serialization.
-    The class immediately resolves to a `pint.Quantity` upon construction.
-    The primary use for `PydanticPintValue` is in `pydantic.Field` comparison restrictions.
+def pydantic_pint_value(
+    value: Number,
+    units: str | None = None,
+    /,
+    *,
+    ureg: pint.UnitRegistry | None = None,
+) -> pint.Quantity:
+    """Construct `pint.Quantity` with an injected Pydantic schema.
+
+    A serialization schema is added to a Pint quantity to allow it to be serialized
+    by pydantic. This in-turn allows Pint values to be used in a `pydantic.Field`
+    context.
+
+    Args:
+        value (Number):
+            The magnitude of the quantity.
+        units (str | None, optional):
+            The units of the quantity.
+            Defaults to unitless quantity.
+        ureg (pint.UnitRegistry | None, optional):
+            The unit registry from which to create the quantity.
+            Defaults to `pydantic_pint.app_registry`.
+
+    Returns:
+        A `pint.Quantity` with pydantic serialization.
     """
+    ureg = ureg if ureg else get_registry()
+    inst = ureg.Quantity(value, units)
 
-    def __new__(
-        cls,
-        __value: Number,
-        __units: str | None = None,
-        /,
-        *,
-        ureg: pint.UnitRegistry | None = None,
-    ) -> pint.Quantity:
-        """Coarse value into a `pint.Quantity` based on provided unit registry.
-
-        Args:
-            __value (Number):
-                The magnitude of the quantity.
-            __units (str | None, optional):
-                The units of the quantity.
-                Defaults to unitless quantity.
-            ureg (pint.UnitRegistry | None, optional):
-                The unit registry from which to create the quantity.
-                Defaults to `pydantic_pint.app_registry`.
-
-        Returns:
-            A `pint.Quantity` with pydantic serialization.
-        """
-        ureg = ureg if ureg else get_registry()
-        inst = ureg.Quantity(__value, __units)
-
-        inst.__pydantic_serializer__ = SchemaSerializer(
-            core_schema.any_schema(
-                serialization=core_schema.plain_serializer_function_ser_schema(
-                    lambda v: str(v),
-                    info_arg=False,
-                    when_used="always",
-                )
+    schema = SchemaSerializer(
+        core_schema.any_schema(
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda v: str(v),
+                info_arg=False,
+                when_used="always",
             )
         )
+    )
 
-        return inst
+    setattr(inst, "__pydantic_serializer__", schema)
+
+    return inst
+
+
+# for compatibility
+PydanticPintValue = pydantic_pint_value
+"""Proxy class for a Pint Quantity instance with pydantic serialization.
+
+!!! warning
+
+    The name `PydanticPintQuantity` is deprecated. Use `pydantic_pint_value` instead.
+
+Unlink `PydanticPintQuantity`, `PydanticPintValue` wraps an instance of a pint
+quantity. Methods are added to allow it to interact with pydantic, e.g. serialization.
+The class immediately resolves to a `pint.Quantity` upon construction. The primary
+use for `PydanticPintValue` is in `pydantic.Field` comparison restrictions.
+
+See `pydantic_pint_value` for more details.
+"""
